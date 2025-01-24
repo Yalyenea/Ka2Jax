9e76c194cc801c7cbaa6fa09d1d95d64ebbf28a0
Yalyenea <yuanfang256@outlook.com>
2025-01-24 16:04:09 +0800

    clip on/off

--- obj/x64/Debug/net6.0-windows/Ka2Jax.AssemblyInfo.cs
+++ obj/x64/Debug/net6.0-windows/Ka2Jax.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ka2Jax")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a76dce53e7df04373105b003207b0be24781d339")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ka2Jax")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ka2Jax")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
